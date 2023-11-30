a0002945c7638bcf70155663ed40a8dd5a7da6f7
tolipva <shahinakhan3009@gmail.com>
2023-11-30 20:03:48 +0500

    static files

--- core/urls.py
+++ core/urls.py
@@ -2,4 +2,18 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', home, name="home"),
+    path('patient-list/',patient_list, name="patient_list"),
+    path('patient-profile/',  patient_profile, name="patient_profile"),
+    path('add-patient/', add_patient, name="add_patient"),
+    path('all-doctors/', all_doctors, name="all_doctors"),
+    path('doctor-profile/', doctor_profile, name="doctor_profile"),
+    path('add-doctor/', add_doctor, name="add_doctor"),
+    path('appointment', appointment, name="appointment"),
+    path('operation/', operation, name="operation"),
+    path('emergency-form/', emergency_form, name="emergency_form"),
+    path('emergency-list/', emergency_list, name="emergency_list"),
+    path('reports/', reports, name="reports"),
+    path('accounts/', accounts, name="accounts"),
+    path('expense-list/', expense_list, name="expense_list"),
+    path('add-expense/', add_expense, name="add_expense")
 ]
--- core/views.py
+++ core/views.py
@@ -2,4 +2,47 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'main.html' )
+    return render(request,'index.html' )
+def patient_list(request):
+    return render(request,'patient/patient-list.html' )
+def patient_profile(request):
+    return render(request,'patient/Patient-Profile.html' )
+def add_patient(request):
+    return render(request,'patient/add-new-patient.html' )
+def all_doctors(request):
+    return render(request,'doctor/all-doctors.html' )
+def doctor_profile(request):
+    return render(request,'doctor/doctor-Profile.html' )
+def add_doctor(request):
+    return render(request,'doctor/add-new-doctor.html' )
+def appointment(request):
+    return render(request,'doctor/appointment.html' )
+def operation(request):
+    return render(request,'operation.html' )
+def emergency_form(request):
+    return render(request,'emergency/emergency-form.html' )
+def emergency_list(request):
+    return render(request,'emergency/emergency-list.html' )
+def reports(request):
+    return render(request, 'reports.html')
+def accounts(request):
+    return render(request, 'accounts.html')
+def expense_list(request):
+    return render(request, 'expense/expense-list.html')
+def add_expense(request):
+    return render(request, 'expense/add-expense.html')
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
